c354a1303a41134b554faf3d417796d61bed1e61
Jess <jess.owen@lightboxdigital.co.uk>
2021-09-30 14:24:35 +0100

    Fixed Math.round JS

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temp");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
-  temperatureElement.innerHTML = math.round(response.data.main.temp);
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
 }
