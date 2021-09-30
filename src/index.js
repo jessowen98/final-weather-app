c0b6b4606c43ffc84b7a60f7138df074f77a21e8
Jess <jess.owen@lightboxdigital.co.uk>
2021-09-30 20:11:32 +0100

    Added current temperature and details

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temp");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 let apiKey = "c1d63dbb464264ab961e588476c61e78";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Birmingham,UK&appid=${apiKey}&units=metric`;
