af76233356e911f3b5cdabfdee045128b166f292
Jess <jess.owen@lightboxdigital.co.uk>
2021-09-30 21:01:23 +0100

    Linked responsive icons

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+//real time date
 function formatDate(timestamp) {
   let date = new Date(timestamp);
   let hours = date.getHours();
@@ -29,14 +30,20 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 let apiKey = "c1d63dbb464264ab961e588476c61e78";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Birmingham,UK&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Lisbon&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
