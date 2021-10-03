42b6962bed98a8b43f31df2bfe8d60f56004385b
Jess <jess.owen@lightboxdigital.co.uk>
2021-10-03 20:15:25 +0100

    API Link

--- src/index.js
+++ src/index.js
@@ -24,28 +24,32 @@ function formatDate(timestamp) {
 }
 //display forecast
 
-function displayForecast() {
+function displayForecast(response) {
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
-  let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
-  days.forEach(function (day) {
+  forecast.forEach(function (forecastDay) {
     forecastHTML =
       forecastHTML +
-      `
-                      
+      `               
                       <div class="col-2">
-                      <div class="forecast-block">
-                        <div class=weather-forecast-date>
-                        ${day} </div>
-                        <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+                        <div class="weather-forecast-date">
+                        ${forecastDay.dt}</div>
+                        <img src="http://openweathermap.org/img/wn/${
+                          forecastDay.weather[0].icon
+                        }@2x.png"
                         width="42"/>
-                        <div class=weather-forecast-temperatures>
-                          <span class=weather-forecast-temp-max>18°</span>
-                          <span class=weather-forecast-temperature-min>12°</span>
+                        <div class="weather-forecast-temperatures">
+                          <span class="weather-forecast-temp-max"> ${Math.round(
+                            forecastDay.temp.max
+                          )}°°</span>
+                          <span class="weather-forecast-temp-min"> ${Math.round(
+                            forecastDay.temp.min
+                          )}° </span>
                       </div>
                     </div>
-                    </div>`;
   });
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
