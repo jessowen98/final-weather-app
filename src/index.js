7c2ff3ccf36475f62fff33917ce33ea4459a6c31
Jess <jess.owen@lightboxdigital.co.uk>
2021-10-05 09:56:24 +0100

    Added placeholder days

--- src/index.js
+++ src/index.js
@@ -23,38 +23,35 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 //display forecast
-
-function displayForecast(response) {
-  let forecast = response.data.daily;
-
+function displayForecast() {
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
-  forecast.forEach(function (forecastDay) {
+  let days = ["Thu", "Fri", "Sat"];
+  days.forEach(function (day) {
     forecastHTML =
       forecastHTML +
-      `               
-                      <div class="col-2">
-                        <div class="weather-forecast-date">
-                        ${forecastDay.dt}</div>
-                        <img src="http://openweathermap.org/img/wn/${
-                          forecastDay.weather[0].icon
-                        }@2x.png"
-                        width="42"/>
-                        <div class="weather-forecast-temperatures">
-                          <span class="weather-forecast-temp-max"> ${Math.round(
-                            forecastDay.temp.max
-                          )}°°</span>
-                          <span class="weather-forecast-temp-min"> ${Math.round(
-                            forecastDay.temp.min
-                          )}° </span>
-                      </div>
-                    </div>`;
+      `
+    <div class="col-2">
+      <div class="weather-forecast-date">${day}</div>
+      <img
+        src="http://openweathermap.org/img/wn/04n@2x.png"
+        alt=""
+        width="42"
+      />
+      <div class="weather-forecast-temp">
+        <span class="weather-forecast-temp-min">18</span>
+        <span class="weather-forecast-temp-max">12</span>
+      </div>
+    </div>
+  
+  `;
   });
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
 
+//display Temp
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -118,3 +115,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Birmingham");
+displayForecast();
